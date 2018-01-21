1a7ee637eb358ed6d715b68f7918b7af22b1444c
jenhaddock <jentx123@hotmail.com>
2018-01-21 14:41:20 -0600

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -7,6 +7,10 @@ import Rating from '../src/components/Rating';
 import Circle from '../src/components/Circle';
 import Animation from '../src/components/Animation';
 import Pikachu from '../src/components/Pikachu';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure ({ adapter: new Adapter()});
 
 
 describe('Rating', () => {
